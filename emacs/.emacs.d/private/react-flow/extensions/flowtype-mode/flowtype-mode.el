7469acc7e1fbf72c214be71afbd050edb083fe67
Florian Diebold <flodiebold@gmail.com>
2015-08-23 16:31:27 +0200

    flowtype-mode: Beginnings of AST walking.

--- emacs/.emacs.d/private/react-flow/extensions/flowtype-mode/flowtype-mode.el
+++ emacs/.emacs.d/private/react-flow/extensions/flowtype-mode/flowtype-mode.el
@@ -115,12 +115,115 @@
   (flowtype//type-at-pos-async #'flowtype//eldoc-show-type-info (point))
   nil)
 
+(defvar flowtype--ast nil
+  "The AST from flow.")
+
+(defun flowtype//receive-ast (data)
+  "Handler for the flow AST call."
+  (message "flowtype//receive-ast called")
+  (let ((ast (json-read-from-string data)))
+    (setq flowtype--ast ast)))
+
+(defun flowtype//do-parse ()
+  "Calls flow to get the AST and stores it in flowtype--ast."
+  (flowtype//call-flow-on-current-buffer-async #'flowtype//receive-ast "ast"))
+
+;; AST functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun flowtype//node-field (node field)
+  "Returns the given field of node."
+  (cdr (assq field node)))
+
+(defun flowtype//node-type (node)
+  "Returns the node type of node."
+  (intern (flowtype//node-field node 'type)))
+
+(defun flowtype//node-body (node)
+  "Returns the body of node."
+  (flowtype//node-field node 'body))
+
+(defun flowtype//node-expression (node)
+  "Returns the expression of node."
+  (flowtype//node-field node 'expression))
+
+(defun flowtype//node-left (node)
+  "Returns the left of node."
+  (flowtype//node-field node 'left))
+
+(defun flowtype//node-right (node)
+  "Returns the right of node."
+  (flowtype//node-field node 'right))
+
+(defun flowtype//node-object (node)
+  "Returns the object of node."
+  (flowtype//node-field node 'object))
+
+(defun flowtype//node-property (node)
+  "Returns the property of node."
+  (flowtype//node-field node 'property))
+
+(defun flowtype//body-p (node-type)
+  "Non-nil if node-type just consists of its body."
+  (memq node-type '(Program BlockStatement)))
+
+(defun flowtype//leaf-p (node-type)
+  "Non-nil if node-type has no children."
+  (memq node-type '(NumberTypeAnnotation StringTypeAnnotation)))
+
+(defun flowtype//visit (fun thing)
+  "If thing is a vector, run fun on each element; otherwise run fun on thing."
+  (cond
+   ((vectorp thing)
+    (loop for child being the elements of thing
+          do (flowtype//visit fun child)))
+   ((null thing) nil)
+   (t (funcall fun thing))))
+
+(defun flowtype//visit-fields (fields fun thing)
+  "Visits all fields of thing with fun."
+  (dolist (field fields)
+    (flowtype//visit fun (flowtype//node-field thing field))))
+
+(defun flowtype//visit-children (fun ast-node)
+  "Runs fun on each of ast-node's children in turn."
+  (pcase (flowtype//node-type ast-node)
+    ((pred flowtype//leaf-p)
+     nil)
+    ((or `AssignmentExpression `BinaryExpression)
+     (flowtype//visit-fields '(left right) fun ast-node))
+    (`ExpressionStatement
+     (flowtype//visit-fields '(expression) fun ast-node))
+    (`MemberExpression
+     (flowtype//visit-fields '(object property) fun ast-node))
+    ((pred flowtype//body-p)
+     (flowtype//visit-fields '(body) fun ast-node))
+    (`FunctionExpression
+     (flowtype//visit-fields '(params returnType body) fun ast-node))
+    ((or `TypeAnnotation `Identifier)
+     (flowtype//visit-fields '(typeAnnotation) fun ast-node))
+    (`ReturnStatement
+     (flowtype//visit-fields '(argument) fun ast-node))
+    (`ExportDeclaration
+     (flowtype//visit-fields '(specifiers declaration) fun ast-node))
+    (`TypeAlias
+     (flowtype//visit-fields '(id typeParameters right) fun ast-node))
+    (`UnionTypeAnnotation
+     (flowtype//visit-fields '(types) fun ast-node))
+    (unknown
+     (message "Unknown node type: %s" unknown))))
+
+(defun flowtype//walk-ast-print-types (ast-node)
+  "Walks the ast, printing node types."
+  (message "Node: %s" (flowtype//node-type ast-node))
+  (flowtype//visit-children #'flowtype//walk-ast-print-types ast-node))
+
 (define-derived-mode flowtype-mode
   web-mode "FlowJSX"
   "Major mode for JSX with flow types."
   (setq web-mode-content-type "jsx")
   (setq web-mode-markup-indent-offset 2)
   (set (make-local-variable 'eldoc-documentation-function) #'flowtype/eldoc-show-type-at-point)
+  (make-local-variable 'flowtype--ast)
   (turn-on-eldoc-mode)
   (flycheck-mode 1))
 
