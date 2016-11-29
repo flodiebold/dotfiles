81d1ae671d65c167bb38d261db3dfdfd6d31902a
Florian Diebold <flodiebold@gmail.com>
2016-11-29 11:27:13 +0100

    Update react-flow to new spacemacs

--- emacs/.emacs.d/private/react-flow/extensions.el
+++
@@ -1,9 +0,0 @@
-;;; extensions.el --- React-flow Layer Extensions File for Spacemacs
-
-(setq react-flow-post-extensions '(flowtype-mode))
-
-(defun react-flow/init-flowtype-mode ()
-  (use-package flowtype-mode
-    :config
-    (progn
-      (evil-leader/set-key-for-mode 'flowtype-mode "gg" 'flowtype/get-def-at-point))))
--- emacs/.emacs.d/private/react-flow/packages.el
+++ emacs/.emacs.d/private/react-flow/packages.el
@@ -5,7 +5,14 @@
                             web-mode
                             flycheck
                             dash
+                            (flowtype-mode :location local)
                             ))
 
 (defun react-flow/init-web-mode ()
   (use-package web-mode))
+
+(defun react-flow/init-flowtype-mode ()
+  (use-package flowtype-mode
+    :config
+    (progn
+      (evil-leader/set-key-for-mode 'flowtype-mode "gg" 'flowtype/get-def-at-point))))
