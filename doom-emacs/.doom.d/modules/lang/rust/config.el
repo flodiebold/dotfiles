7b4742cc8f326fb955c5b6c132617f87513e95b2
Florian Diebold <flodiebold@gmail.com>
2021-07-13 19:00:33 +0200

    More rust-mode improvements

--- doom-emacs/.doom.d/modules/lang/rust/config.el
+++ doom-emacs/.doom.d/modules/lang/rust/config.el
@@ -3,9 +3,21 @@
 (after! projectile
   (add-to-list 'projectile-project-root-files "Cargo.toml"))
 
+;; work around bug with disabled optional libraries
+(defun rust-before-save-hook ())
+(defun rust-after-save-hook ())
+
 (use-package! rust-mode
   :mode ("\\.rs$" . rust-mode)
   :init
   (setq rust-load-optional-libraries nil)
   :config
-  (add-hook 'rust-mode-hook #'lsp!))
+  (add-hook 'rust-mode-hook #'lsp!)
+  (define-key rust-mode-map [remap evil-join] 'lsp-rust-analyzer-join-lines)
+  (map! :map rust-mode-map
+        :localleader
+        (:prefix ("r" . "run")
+         :desc "run" "r" #'lsp-rust-analyzer-run
+         :desc "run again" "a" #'lsp-rust-analyzer-rerun)
+        (:prefix ("m" . "macro")
+         :desc "expand macro" "x" #'lsp-rust-analyzer-expand-macro)))
